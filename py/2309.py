feeb7e53dcbd0c5fd8d8e02753eae11572b33e9a
k-minsik <smin58810@gmail.com>
2023-10-20 17:35:57 +0900

    Update: BOJ 2309 remind

--- py/2309.py
+++ py/2309.py
@@ -1,18 +1,34 @@
-from itertools import combinations
-arr = []
-res = 0
+# from itertools import combinations
+# arr = []
+# res = 0
 
-for i in range(9):
-    temp = int(input())
-    res += temp
-    arr.append(temp)
+# for i in range(9):
+#     temp = int(input())
+#     res += temp
+#     arr.append(temp)
 
-for i in list(combinations(arr, 2)):
-    if res - i[0] - i[1] == 100:
-        arr.remove(i[0])
-        arr.remove(i[1])
-        break
+# for i in list(combinations(arr, 2)):
+#     if res - i[0] - i[1] == 100:
+#         arr.remove(i[0])
+#         arr.remove(i[1])
+#         break
     
-arr = sorted(arr, reverse=False)
+# arr = sorted(arr, reverse=False)
 # for i in arr:
-print(i)
+#     print(i)
+
+import sys
+input = sys.stdin.readline
+
+graph = [int(input()) for _ in range(9)]
+ret = sum(graph)
+
+for i in range(8):
+    for j in range(i+1, 9):
+        if ret - graph[i] - graph[j] == 100:
+            del graph[j]
+            del graph[i]
+            
+            for i in sorted(graph):
+                print(i)
+            exit()
