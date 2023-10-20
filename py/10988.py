3616748f3c7855cdce56b05567cde8fe4370e05f
k-minsik <smin58810@gmail.com>
2023-10-20 17:51:10 +0900

    Update: BOJ 10988 remind

--- py/10988.py
+++ py/10988.py
@@ -1,2 +1,8 @@
-s = input().strip()
+# s = input().strip()
+# print(1) if s == s[::-1] else print(0)
+
+import sys
+input = sys.stdin.readline
+s = input().rstrip()
+
 print(1) if s == s[::-1] else print(0)
