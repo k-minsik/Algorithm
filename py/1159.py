9f5997717016313e987a8d1ef19058cbf586f5d4
k-minsik <smin58810@gmail.com>
2023-10-20 17:55:53 +0900

    Update: BOJ 1159 remind

--- py/1159.py
+++ py/1159.py
@@ -1,17 +1,35 @@
+# n = int(input())
+
+# cnt = {}
+# res = ''
+# for _ in range(n):
+#     mem = input().strip()
+#     if mem[0] in cnt:
+#         cnt[mem[0]] += 1
+#     else:
+#         cnt[mem[0]] = 1
+
+# cnt = dict(sorted(cnt.items(), reverse=False))
+# for k, v in cnt.items():
+#     if v > 4:
+#         res += k
+
+# print(res) if res else print('PREDAJA')
+
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+count = [0 for _ in range(26)]
 
-cnt = {}
-res = ''
 for _ in range(n):
-    mem = input().strip()
-    if mem[0] in cnt:
-        cnt[mem[0]] += 1
-    else:
-        cnt[mem[0]] = 1
+    name = input().rstrip()
+    count[ord(name[0]) - 97] += 1
 
-cnt = dict(sorted(cnt.items(), reverse=False))
-for k, v in cnt.items():
-    if v > 4:
-        res += k
+ret = ""
+for i in range(26):
+    if count[i] >= 5:
+        ret += chr(i + 97)
 
-print(res) if res else print('PREDAJA')
+print(ret) if ret != "" else print("PREDAJA")
