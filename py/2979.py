14dd361098fa11ca4eb0d51ff609fae8d7b3ef26
k-minsik <smin58810@gmail.com>
2023-10-20 17:49:06 +0900

    Update: BOJ 2979 remind

--- py/2979.py
+++ py/2979.py
@@ -1,22 +1,44 @@
+# import sys
+# input = sys.stdin.readline
+
+# fee = 0
+# arr = [0 for _ in range(101)]
+# a, b, c = map(int, input().split())
+
+# for _ in range(3):
+#     t1, t2 = map(int, input().split())
+    
+#     for i in range(t1, t2):
+#         arr[i] += 1
+
+# for i in arr:
+#     if i == 1:
+#         fee += a
+#     elif i == 2:
+#         fee += b*2
+#     elif i == 3:
+#         fee += c*3
+
+# print(fee)
+
 import sys
 input = sys.stdin.readline
 
-fee = 0
-arr = [0 for _ in range(101)]
+ret = 0
+parked = [0 for _ in range(101)]
+
 a, b, c = map(int, input().split())
-
 for _ in range(3):
-    t1, t2 = map(int, input().split())
-    
-    for i in range(t1, t2):
-        arr[i] += 1
+    s, e = map(int, input().split())
+    for i in range(s, e):
+        parked[i] += 1
 
-for i in arr:
+for i in parked:
     if i == 1:
-        fee += a
+        ret += a
     elif i == 2:
-        fee += b*2
+        ret += b*2
     elif i == 3:
-        fee += c*3
+        ret += c*3
 
-print(fee)
+print(ret)
