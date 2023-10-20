bab84298391b901f5aa55f5c4a01cd014a80d249
k-minsik <smin58810@gmail.com>
2023-10-20 17:36:38 +0900

    Update: BOJ 10808 remind

--- py/10808.py
+++ py/10808.py
@@ -1,12 +1,23 @@
+# import sys
+# input = sys.stdin.readline
+
+# cnt = [0] * 26
+# s = list(input().strip())
+
+# for i in s:
+#     cnt[ord(i) - 97] += 1
+
+# for i in cnt:
+#     print(i, end=' ')
+    
 import sys
 input = sys.stdin.readline
 
-cnt = [0] * 26
-s = list(input().strip())
+s = input().rstrip()
+count = [0 for _ in range(26)]
 
 for i in s:
-    cnt[ord(i) - 97] += 1
+    count[ord(i) - 97] += 1
 
-for i in cnt:
-    print(i, end=' ')
-    
+for i in count:
+    print(i, end = ' ')
