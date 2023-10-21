00343217c3cbe7aad03e0a8b83d5ef4d79e89ba1
k-minsik <smin58810@gmail.com>
2023-10-21 14:13:25 +0900

    Update: BOJ 2559 remind

--- py/2559.py
+++ py/2559.py
@@ -1,14 +1,30 @@
-n, t = map(int, input().split())
-temp = list(map(int, input().split()))
-presum = [0]
+# n, t = map(int, input().split())
+# temp = list(map(int, input().split()))
+# presum = [0]
 
-for i in range(n):
-    presum.append(presum[-1] + temp[i])
+# for i in range(n):
+#     presum.append(presum[-1] + temp[i])
 
-M = -1e9
-for i in range(t, n+1):
-    res = presum[i] - presum[i-t]
-    M = max(M, res)
+# M = -1e9
+# for i in range(t, n+1):
+#     res = presum[i] - presum[i-t]
+#     M = max(M, res)
 
-print(M)
+# print(M)
 
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+temperature = list(map(int, input().split()))
+accumulatedSum = [0] + [i for i in temperature]
+
+maxSum = -1e9
+for i in range(1, n + 1):
+    accumulatedSum[i] += accumulatedSum[i - 1]
+
+    if i >= k:
+        maxSum = max(maxSum, accumulatedSum[i] - accumulatedSum[i - k])
+
+print(maxSum)
