c40b8f429863136e63c4eb3585b1335f083026aa
k-minsik <smin58810@gmail.com>
2023-10-21 13:56:05 +0900

    Update: BOJ 9996 remind

--- py/9996.py
+++ py/9996.py
@@ -1,12 +1,29 @@
+# n = int(input())
+# s, f = input().split('*')
+
+# for _ in range(n):
+#     temp = input().rstrip()
+#     if len(temp) >= len(s) + len(f):
+#         if temp[0:len(s)] == s and temp[-len(f):] == f:
+#             print('DA')
+#         else:
+#             print('NE')
+#     else:
+#         print('NE')
+
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-s, f = input().split('*')
+s, e = input().rstrip().split('*')
 
 for _ in range(n):
     temp = input().rstrip()
-    if len(temp) >= len(s) + len(f):
-        if temp[0:len(s)] == s and temp[-len(f):] == f:
+
+    if len(temp) >= len(s) +len(e):
+        if temp.startswith(s) and temp.endswith(e):
             print('DA')
-        else:
-            print('NE')
-    else:
-        print('NE')
+            continue
+        
+    print('NE')
