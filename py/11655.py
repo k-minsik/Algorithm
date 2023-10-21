e4ae5047d8344e8b61f36a7acac3c11f16e4c911
k-minsik <smin58810@gmail.com>
2023-10-21 13:37:41 +0900

    Update: BOJ 11655 remind

--- py/11655.py
+++ py/11655.py
@@ -1,17 +1,31 @@
-res = ''
+# res = ''
+# s = list(input().rstrip())
+
+# for i in range(len(s)):
+#     if s[i].islower():
+#         temp = ord(s[i]) + 13
+#         if temp > 122:
+#             temp -= 26
+#         s[i] = chr(temp)
+#     elif s[i].isupper():
+#         temp = ord(s[i]) + 13
+#         if temp > 90:
+#             temp -= 26
+#         s[i] = chr(temp)
+    
+#     print(s[i], end='')
+
+
+import sys
+input = sys.stdin.readline
+
 s = list(input().rstrip())
 
 for i in range(len(s)):
-    if s[i].islower():
-        temp = ord(s[i]) + 13
-        if temp > 122:
-            temp -= 26
-        s[i] = chr(temp)
-    elif s[i].isupper():
-        temp = ord(s[i]) + 13
-        if temp > 90:
-            temp -= 26
-        s[i] = chr(temp)
-    
-    print(s[i], end='')
+    # A-Z = 65-90 , a-z = 97-122
+    if 'A' <= s[i] <= 'Z':
+        s[i] = chr((ord(s[i]) - ord('A') + 13) % 26 + ord('A'))
+    elif 'a' <= s[i] <= 'z':
+        s[i] = chr((ord(s[i]) - ord('a') + 13) % 26 + ord('a'))
 
+print(''.join(s))
