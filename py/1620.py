7ebb07d35d2dd09aaab29150ac55e6853eb8d137
k-minsik <smin58810@gmail.com>
2023-10-22 18:52:32 +0900

    Update: BOJ 1620 remind

--- py/1620.py
+++ py/1620.py
@@ -1,18 +1,38 @@
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# mon = [0]
+# mon2 = {}
+
+# for i in range(1, n+1):
+#     temp = input().rstrip()
+#     mon.append(temp)
+#     mon2[temp] = i
+
+# for _ in range(m):
+#     quiz = input().rstrip()
+#     if quiz[0].isalpha():
+#         print(mon2[quiz])
+#     else:
+#         print(mon[int(quiz)])
+
+
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-mon = [0]
-mon2 = {}
+pokemonDict = {}
+pokemonArray = ['']
 
-for i in range(1, n+1):
-    temp = input().rstrip()
-    mon.append(temp)
-    mon2[temp] = i
+for i in range(1, n + 1):
+    name = input().rstrip()
+    pokemonDict[name] = i
+    pokemonArray.append(name)
 
 for _ in range(m):
     quiz = input().rstrip()
-    if quiz[0].isalpha():
-        print(mon2[quiz])
+    if quiz.isdigit():
+        print(pokemonArray[int(quiz)])
     else:
-        print(mon[int(quiz)])
+        print(pokemonDict(quiz))
