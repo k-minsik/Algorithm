5dae2238b506a4e9b73d46e8c0cbd26eef2a0e90
k-minsik <smin58810@gmail.com>
2023-10-22 20:32:34 +0900

    Update: BOJ 3986 remind

--- py/3986.py
+++ py/3986.py
@@ -1,23 +1,48 @@
-res = 0
+# res = 0
 
+# n = int(input())
+# for _ in range(n):
+#     stack = []
+#     s = list(input().rstrip())
+#     stack.append(s.pop())
+
+#     while s:
+#         temp = s.pop()
+#         if stack:
+#             if stack[-1] == temp:
+#                 stack.pop()
+#             else:
+#                 stack.append(temp)
+#         else:
+#             stack.append(temp)
+        
+        
+#     if not stack: res += 1
+
+# print(res)
+
+
+
+import sys
+input = sys.stdin.readline
+
+answer = 0 
 n = int(input())
 for _ in range(n):
-    stack = []
-    s = list(input().rstrip())
-    stack.append(s.pop())
+    word = list(input().rstrip())
+    stack = [word.pop()]
 
-    while s:
-        temp = s.pop()
+    while word:
         if stack:
-            if stack[-1] == temp:
+            if stack[-1] == word[-1]:
                 stack.pop()
+                word.pop()
             else:
-                stack.append(temp)
+                stack.append(word.pop())
         else:
-            stack.append(temp)
-        
-        
-    if not stack: res += 1
+            stack.append(word.pop())
 
-print(res)
+    if not stack:
+        answer += 1
 
+print(answer)
