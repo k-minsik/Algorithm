cc62b2372edf9c77f6bdc92c4c25d61ceb91d442
k-minsik <smin58810@gmail.com>
2023-10-22 20:01:34 +0900

    Update: BOJ 1213 remind

--- py/1213.py
+++ py/1213.py
@@ -1,21 +1,48 @@
-arr = [0 for _ in range(26)]
+# arr = [0 for _ in range(26)]
 
-s = input().rstrip()
+# s = input().rstrip()
 
-for i in range(len(s)):
-    arr[ord(s[i]) - 65] += 1
+# for i in range(len(s)):
+#     arr[ord(s[i]) - 65] += 1
 
-flag = 0
-res = ''
+# flag = 0
+# res = ''
+# mid = ''
+
+# for i in range(26):
+#     if arr[i] % 2 == 1:
+#         flag += 1
+#         if flag == 2:
+#             break
+#         mid = chr(i + 65)
+#     res += chr(i+65) * (arr[i] //2)
+
+# if flag == 2: print("I'm Sorry Hansoo")
+# else: print(res + mid + res[::-1])
+
+
+
+
+import sys
+input = sys.stdin.readline
+
+name = list(input().rstrip())
+count = [0 for _ in range(26)]
+
+for i in name:
+    count[ord(i) - ord('A')] += 1
+
+answer = ''
 mid = ''
-
 for i in range(26):
-    if arr[i] % 2 == 1:
-        flag += 1
-        if flag == 2:
-            break
-        mid = chr(i + 65)
-    res += chr(i+65) * (arr[i] //2)
+    c = chr(i + ord('A'))
+    if count[i] % 2 == 1:
+        if mid:
+            print("I'm Sorry Hansoo")
+            exit()
+        else:
+            mid = c
+    
+    answer += c * (count[i] // 2)
 
-if flag == 2: print("I'm Sorry Hansoo")
-else: print(res + mid + res[::-1])
+print(answer + mid + answer[::-1])
