f2f1c189f066cc1e3a2bf9237b55b3ea79626b29
k-minsik <smin58810@gmail.com>
2023-10-23 18:22:03 +0900

    refactor: BOJ 3474

--- py/3474.py
+++ py/3474.py
@@ -1,15 +1,35 @@
+# import sys
+# input = sys.stdin.readline
+# n = int(input());
+
+# for _ in range(n):
+#     num = int(input())
+
+#     cnt = 0
+#     i = 5
+    
+#     while i <= num:
+#         cnt += num // i
+#         i *= 5
+
+#     print(cnt)
+
+
+
 import sys
 input = sys.stdin.readline
-n = int(input());
 
-for _ in range(n):
-    num = int(input())
+tc = int(input())
+for _ in range(tc):
+    n = int(input())
 
-    cnt = 0
-    i = 5
-    
-    while i <= num:
-        cnt += num // i
-        i *= 5
+    answer = 0
+    five = 5
+    while True:
+        if five > n:
+            break
+        
+        answer += n // five
+        five *= 5
 
-    print(cnt)
+    print(answer)
