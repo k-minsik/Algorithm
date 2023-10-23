58d1387fac0f6e15be87390ee3990c4598085ad5
k-minsik <smin58810@gmail.com>
2023-10-23 15:34:02 +0900

    Update: BOJ 1629 remind

--- py/1629.py
+++ py/1629.py
@@ -1,18 +1,34 @@
-a, b, c = map(int, input().split())
+# a, b, c = map(int, input().split())
 
-def go(a, b):
+# def go(a, b):
+#     if b == 1:
+#         return a % c
+
+#     res = go(a, b // 2)
+#     res = (res * res) % c
+
+#     if b % 2 == 1:
+#         res = (res * a) % c
+
+#     return res
+
+
+# print(go(a, b))
+
+
+import sys
+input = sys.stdin.readline
+
+def func(a, b, c):
     if b == 1:
         return a % c
+    
+    next = func(a, b // 2, c)
 
-    res = go(a, b // 2)
-    res = (res * res) % c
+    if b % 2 == 0:
+        return next * next % c
+    else:
+        return a * next * next % c
 
-    if b % 2 == 1:
-        res = (res * a) % c
-
-    return res
-
-
-print(go(a, b))
-
-pow()
+a, b, c = map(int, input().split())
+print(func(a, b, c))
