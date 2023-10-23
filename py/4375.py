a7d01e0cb395880c4f40f5e372a776b4c8e50368
k-minsik <smin58810@gmail.com>
2023-10-23 15:40:04 +0900

    Update: BOJ 4375 remind

--- py/4375.py
+++ py/4375.py
@@ -1,15 +1,35 @@
+# while True:
+#     try:
+#         n = int(input())
+#     except:
+#         break
+
+#     temp = 1
+#     cnt = 1
+#     while True:
+#         if temp % n == 0:
+#             print(cnt)
+#             break
+#         else:
+#             temp = (temp * 10 + 1) % n
+#             cnt += 1
+
+import sys
+input = sys.stdin.readline
+
 while True:
     try:
         n = int(input())
     except:
         break
 
-    temp = 1
-    cnt = 1
+    compare = 1
+    count = 1
     while True:
-        if temp % n == 0:
-            print(cnt)
+        if compare % n == 0:
             break
-        else:
-            temp = (temp * 10 + 1) % n
-            cnt += 1
+
+        compare = (compare * 10 + 1) % n
+        count += 1
+    
+    print(count)
