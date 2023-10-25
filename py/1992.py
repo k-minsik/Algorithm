e6e088a21695b77ef382ffa85e3be0f7bfdceb3b
k-minsik <smin58810@gmail.com>
2023-10-25 18:37:48 +0900

    refactor: Remind BOJ #1992

--- py/1992.py
+++ py/1992.py
@@ -1,26 +1,52 @@
+# n = int(input())
+# arr = [list(map(int, input())) for _ in range(n)]
+
+# def f(y, x, size):
+#     temp = ""
+#     char = str(arr[y][x])
+#     if size == 1:
+#         return str(arr[y][x])
+#     else:
+#         for i in range(y, y + size):
+#             for j in range(x, x + size):
+#                 if(str(arr[i][j]) != char):
+#                     temp += "("
+#                     temp += f(y, x, size//2)
+#                     temp += f(y, x + size//2, size//2)
+#                     temp += f(y + size//2, x, size//2)
+#                     temp += f(y + size//2, x + size//2, size//2)
+#                     temp += ")"
+#                     return temp
+
+#     return str(arr[y][x])
+
+# res = f(0, 0, n)
+
+# print(res)
+
+
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = [list(map(int, input())) for _ in range(n)]
+graph = [list(input().rstrip()) for _ in range(n)]
 
-def f(y, x, size):
-    temp = ""
-    char = str(arr[y][x])
-    if size == 1:
-        return str(arr[y][x])
-    else:
-        for i in range(y, y + size):
-            for j in range(x, x + size):
-                if(str(arr[i][j]) != char):
-                    temp += "("
-                    temp += f(y, x, size//2)
-                    temp += f(y, x + size//2, size//2)
-                    temp += f(y + size//2, x, size//2)
-                    temp += f(y + size//2, x + size//2, size//2)
-                    temp += ")"
-                    return temp
+def recursion(y, x, n):
+    if n == 1:
+        return graph[y][x]
+    
+    for i in range(y, y + n):
+        for j in range(x, x + n):
+            if graph[i][j] != graph[y][x]:
+                ret = "("
+                ret += recursion(y, x, n // 2)
+                ret += recursion(y, x + (n // 2), n // 2)
+                ret += recursion(y + (n // 2), x, n // 2)
+                ret += recursion(y + (n // 2), x + (n // 2), n // 2)
+                ret += ")"
+                return ret
 
-    return str(arr[y][x])
+    return graph[y][x]
 
-res = f(0, 0, n)
-
-print(res)
-
+print(recursion(0, 0, n))
