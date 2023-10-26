e32c901eb5b00324621c4f1c11467d8518b3ce9c
k-minsik <smin58810@gmail.com>
2023-10-26 19:01:10 +0900

    refactor: Remind BOJ #2910

--- py/2910.py
+++ py/2910.py
@@ -1,17 +1,36 @@
+# n, c = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# cnt = {}
+
+# for i in range(len(arr)):
+#     if arr[i] in cnt:
+#         cnt[arr[i]][0] += 1
+#     else:
+#         cnt[arr[i]] = [1, i]
+
+# cnt = sorted(cnt.items(), key = lambda x: (-x[1][0], x[1][1]))
+
+# for k, v in cnt:
+#     for _ in range(v[0]):
+#         print(k, end = ' ')
+
+
+import sys
+input = sys.stdin.readline
+
 n, c = map(int, input().split())
-arr = list(map(int, input().split()))
+array = list(map(int, input().split()))
 
-cnt = {}
-
-for i in range(len(arr)):
-    if arr[i] in cnt:
-        cnt[arr[i]][0] += 1
+count = {}
+for i in range(n):
+    if array[i] in count:
+        count[array[i]][1] += 1
     else:
-        cnt[arr[i]] = [1, i]
+        count[array[i]] = [i, 1]
 
-cnt = sorted(cnt.items(), key = lambda x: (-x[1][0], x[1][1]))
+answer = ""
+for k, v in sorted(count.items(), key = lambda x : (-x[1][1], x[1][0])):
+    answer += (str(k) + " ") * v[1]
 
-for k, v in cnt:
-    for _ in range(v[0]):
-        print(k, end = ' ')
-
+print(answer.rstrip())
