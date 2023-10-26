f90e4d339e77cf2c8c869027e4bc679f7a2bd7ef
k-minsik <smin58810@gmail.com>
2023-10-26 18:29:49 +0900

    refactor: Remind BOJ #2828

--- py/2828.py
+++ py/2828.py
@@ -1,26 +1,52 @@
+# n, m = map(int, input().split())
+# j = int(input())
+
+# b_l = 1
+# b_r = m
+
+# answer = 0
+
+# for k in range(j):
+#     w = int(input())
+
+#     if b_l <= w <= b_r:
+#         continue
+#     elif 1 <= w < b_l:
+#         temp = b_l - w
+#         b_l -= temp
+#         b_r -= temp
+#         answer += temp
+#     elif b_r < w <= n:
+#         temp = w - b_r
+#         b_l += temp
+#         b_r += temp
+#         answer += temp
+
+# print(answer)
+
+
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 j = int(input())
 
-b_l = 1
-b_r = m
+l, r = 1, m
 
 answer = 0
-
-for k in range(j):
-    w = int(input())
-
-    if b_l <= w <= b_r:
+for _ in range(j):
+    target = int(input())
+    
+    if l <= target <= r:
         continue
-    elif 1 <= w < b_l:
-        temp = b_l - w
-        b_l -= temp
-        b_r -= temp
-        answer += temp
-    elif b_r < w <= n:
-        temp = w - b_r
-        b_l += temp
-        b_r += temp
-        answer += temp
+
+    if target < l:
+        move = target - l
+    elif target > r:
+        move = target - r
+
+    l += move
+    r += move
+    answer += abs(move)
 
 print(answer)
-
