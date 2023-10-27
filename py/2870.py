13611e8693d20362701513d9756addd788b275fb
k-minsik <smin58810@gmail.com>
2023-10-27 14:59:32 +0900

    refactor: Remind BOJ #2870

--- py/2870.py
+++ py/2870.py
@@ -1,22 +1,46 @@
-n = int(input())
+# n = int(input())
 
-answer = []
+# answer = []
+# for _ in range(n):
+#     s = input().rstrip();
+
+#     temp = ""
+#     for i in range(len(s)):
+#         if s[i].isalpha():
+#             if temp:
+#                 answer.append(int(temp))
+#                 temp = ""
+#         else:
+#             temp += s[i]
+    
+#     if temp:
+#         answer.append(int(temp))
+
+# answer.sort()
+
+# for i in answer:
+#     print(i)
+
+
+
+import sys
+input = sys.stdin.readline
+
+num = []
+
+n = int(input())
 for _ in range(n):
-    s = input().rstrip();
+    s = input().rstrip() + "e"
+
 
     temp = ""
     for i in range(len(s)):
-        if s[i].isalpha():
-            if temp:
-                answer.append(int(temp))
-                temp = ""
-        else:
+        if s[i].isdigit():
             temp += s[i]
-    
-    if temp:
-        answer.append(int(temp))
-
-answer.sort()
-
-for i in answer:
+        else:
+            if temp:
+                num.append(int(temp))
+                temp = ""
+            
+for i in sorted(num):
     print(i)
