3f37cdb71842a78c33f5007eb81a15e18c89a579
k-minsik <smin58810@gmail.com>
2023-10-29 18:26:46 +0900

    refactor: Remind BOJ #1436

--- py/1436.py
+++ py/1436.py
@@ -1,11 +1,26 @@
+# n = int(input())
+
+# i = 0
+# num = 666
+# while True:
+#     if str(num).find('666') >= 0: i += 1
+#     if i == n:
+#         break
+#     num += 1
+
+# print(num)
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 i = 0
-num = 666
-while True:
-    if str(num).find('666') >= 0: i += 1
-    if i == n:
-        break
-    num += 1
+answer = 666
+while i < n:
+    if str(answer).find("666") >= 0:
+        i += 1
 
-print(num)
+    answer += 1
+
+print(answer - 1)
