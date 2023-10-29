eb2dd7ae53bed13cf736f7ed1687735a7bdb8cd4
k-minsik <smin58810@gmail.com>
2023-10-29 18:41:08 +0900

    refactor: Remind BOJ #9012

--- py/9012.py
+++ py/9012.py
@@ -1,26 +1,53 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+
+# for _ in range(n):
+#     st = []
+#     arr = list(input().rstrip())
+
+#     while arr:
+#         temp = arr.pop()
+        
+#         if st:
+#             if st[-1] != temp:
+#                 st.pop()
+#             else:
+#                 st.append(temp)
+#         else:
+#             if temp == '(':
+#                 st.append(temp)
+#                 break
+#             else:
+#                 st.append(temp)
+
+#     if st: print("NO")
+#     else: print("YES")
+
+
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+def check(string):
+    stack = []
 
-for _ in range(n):
-    st = []
-    arr = list(input().rstrip())
-
-    while arr:
-        temp = arr.pop()
-        
-        if st:
-            if st[-1] != temp:
-                st.pop()
-            else:
-                st.append(temp)
+    for i in string:
+        if i == '(':
+            stack.append(i)
         else:
-            if temp == '(':
-                st.append(temp)
-                break
+            if stack and stack[-1] == '(':
+                stack.pop()
             else:
-                st.append(temp)
+                return False
+    
+    if stack:
+        return False
+    
+    return True
 
-    if st: print("NO")
-    else: print("YES")
+
+n = int(input())
+for _ in range(n):
+    string = input().rstrip()
+    print("YES") if check(string) else print("NO")
