8a552abe759d32b6102f47fc645db6aa5ab483cc
k-minsik <smin58810@gmail.com>
2023-10-29 18:49:13 +0900

    refactor: Remind BOJ #4949

--- py/4949.py
+++ py/4949.py
@@ -1,29 +1,64 @@
-arr = ['(', ')', '[', ']']
+# arr = ['(', ')', '[', ']']
+
+# while True:
+#     s = input().rstrip()
+#     if s == ".": break
+#     s = list(s)
+
+#     st = []
+#     while s:
+#         temp = s.pop()
+#         if temp in arr:
+#             if st:
+#                 if (st[-1] == ")" and temp == "(") or (st[-1] == "]" and temp == "["):
+#                     st.pop()
+#                 else:
+#                     if temp == "(" or temp == "[":
+#                         st.append(temp)
+#                         break
+#                     else:
+#                         st.append(temp)
+#             else:
+#                 if temp == "(" or temp == "[":
+#                         st.append(temp)
+#                         break
+#                 else:
+#                     st.append(temp)
+    
+#     if st: print("no")
+#     else: print("yes")
+
+import sys
+input = sys.stdin.readline
+
+def check(string):
+    stack = []
+
+    for i in string:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                return False
+        elif i == '[':
+            stack.append(i)
+        elif i == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False
+    
+    if stack:
+        return False
+    
+    return True
+
 
 while True:
-    s = input().rstrip()
-    if s == ".": break
-    s = list(s)
-
-    st = []
-    while s:
-        temp = s.pop()
-        if temp in arr:
-            if st:
-                if (st[-1] == ")" and temp == "(") or (st[-1] == "]" and temp == "["):
-                    st.pop()
-                else:
-                    if temp == "(" or temp == "[":
-                        st.append(temp)
-                        break
-                    else:
-                        st.append(temp)
-            else:
-                if temp == "(" or temp == "[":
-                        st.append(temp)
-                        break
-                else:
-                    st.append(temp)
+    string = input().rstrip()
+    if string == ".":
+        break
     
-    if st: print("no")
-    else: print("yes")
+    print("yes") if check(string) else print("no")
