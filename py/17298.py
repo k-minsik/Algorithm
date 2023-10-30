4616167537313b153985d99bcfe869de0edc85b4
k-minsik <smin58810@gmail.com>
2023-10-30 11:01:50 +0900

    refactor: Remind BOJ #17298

--- py/17298.py
+++ py/17298.py
@@ -1,18 +1,37 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# answer = [-1 for _ in range(n)]
+# st = []
+
+# for i in range(n):
+#     temp = arr[i]
+
+#     while st and arr[st[-1]] < temp:
+#         answer[st.pop()] = temp
+
+#     st.append(i)
+    
+# for i in answer:
+#     print(i, end = " ")
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
 answer = [-1 for _ in range(n)]
-st = []
-
+stack = []
 for i in range(n):
-    temp = arr[i]
+    now = nums[i]
 
-    while st and arr[st[-1]] < temp:
-        answer[st.pop()] = temp
+    while stack and nums[stack[-1]] < now:
+        answer[stack.pop()] = now
 
-    st.append(i)
-    
-for i in answer:
-    print(i, end = " ")
+    stack.append(i)
+
+print(*answer)
