b9e30305e156578b57108c43f08d63681938d0cc
k-minsik <smin58810@gmail.com>
2023-10-30 10:49:12 +0900

    refactor: Remind BOJ #1068

--- py/1068.py
+++ py/1068.py
@@ -1,37 +1,72 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# t = [[] for _ in range(n)]
+# temp = list(map(int, input().split()))
+# e = int(input())
+
+
+# for i in range(n):
+#     if temp[i] == -1:
+#         root = i
+#     else:
+#         t[temp[i]].append(i)
+
+# answer = 0 
+# st = [root]
+# if e == root: print(0)
+# else:
+#     while st:
+#         cur = st.pop()
+#         if len(t[cur]) != 0:
+#             if len(t[cur]) == 1 and t[cur][0] == e:
+#                 print(t[cur])
+#                 answer += 1
+#             else:
+#                 for c in t[cur]:
+#                     if c != e:
+#                         st.append(c)
+#         else:
+#             answer += 1
+
+#     print(answer)
+
+# print(t)
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-t = [[] for _ in range(n)]
-temp = list(map(int, input().split()))
-e = int(input())
-
+tree = list(map(int, input().split()))
+graph = [[] for _ in range(n)]
 
 for i in range(n):
-    if temp[i] == -1:
+    if tree[i] == -1:
         root = i
     else:
-        t[temp[i]].append(i)
+        graph[tree[i]].append(i)
 
-answer = 0 
-st = [root]
-if e == root: print(0)
-else:
-    while st:
-        cur = st.pop()
-        if len(t[cur]) != 0:
-            if len(t[cur]) == 1 and t[cur][0] == e:
-                print(t[cur])
-                answer += 1
-            else:
-                for c in t[cur]:
-                    if c != e:
-                        st.append(c)
-        else:
-            answer += 1
+deleteNode = int(input())
+if deleteNode == 0:
+    print(0)
+    exit()
 
-    print(answer)
+parentNode = tree[deleteNode]
+graph[parentNode].remove(deleteNode)
 
-print(t)
+answer = 0
+stack = [root]
+while stack:
+    now = stack.pop()
 
-        
+    if len(graph[now]) == 0:
+        answer += 1
+        continue
+
+    for child in graph[now]:
+        stack.append(child)
+
+print(tree, graph)
+print(answer)
