cd88165e6d14d122bc31bf151b6f16dcc8663247
k-minsik <smin58810@gmail.com>
2023-11-02 12:50:28 +0900

    refactor: Remind BOJ #2109

--- py/2109.py
+++ py/2109.py
@@ -1,22 +1,48 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+# arr.sort(key = lambda x : (-x[0], -x[1]))
+# visited = [0 for _ in range(10001)]
+# visited[0] = 1
+
+# answer = 0
+# for p, d in arr:
+#     while True:
+#         if d == 0:
+#             break
+#         if not visited[d]:
+#             visited[d] = 1
+#             answer += p
+#             break
+
+#         d -= 1
+
+# print(answer)
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-arr.sort(key = lambda x : (-x[0], -x[1]))
-visited = [0 for _ in range(10001)]
-visited[0] = 1
+tutor = [list(map(int, input().split())) for _ in range(n)]
+tutor.sort(key = lambda x : (-x[0], -x[1]))
+visited = [1] + [0 for _ in range(10000)]
 
 answer = 0
-for p, d in arr:
-    while True:
-        if d == 0:
-            break
+for p, d in tutor:
+    if not visited[d]:
+        visited[d] = 1
+        answer += p
+        continue
+
+    while d > 1:
+        d -= 1
+
         if not visited[d]:
             visited[d] = 1
             answer += p
             break
-
-        d -= 1
 
 print(answer)
