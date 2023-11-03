a046767f75019219296979cece8b7603fb2fcf8c
k-minsik <smin58810@gmail.com>
2023-11-03 17:24:44 +0900

    refactor: Remind BOJ #9935

--- py/9935.py
+++ py/9935.py
@@ -1,17 +1,36 @@
-s = input()
-b = input()
-st = []
-l = len(b)
+# s = input()
+# b = input()
+# st = []
+# l = len(b)
 
-for i in range(len(s)):
-    st.append(s[i])
+# for i in range(len(s)):
+#     st.append(s[i])
 
-    if ''.join(st[-l:]) == b:
-        for _ in range(l):
-            st.pop()
+#     if ''.join(st[-l:]) == b:
+#         for _ in range(l):
+#             st.pop()
 
 
-if st:
-    print(''.join(st))
+# if st:
+#     print(''.join(st))
+# else:
+#     print("FRULA")
+
+import sys
+input = sys.stdin.readline
+
+string = input().rstrip()
+deleteString = input().rstrip()
+
+stack = []
+for i in range(len(string)):
+    stack.append(string[i])
+
+    if ''.join(stack[-len(deleteString):]) == deleteString:
+        for _ in range(len(deleteString)):
+            stack.pop()
+        
+if stack:
+    print(''.join(stack))
 else:
     print("FRULA")
