4458391420f13967cf90c1facd7d7c2c066cf289
k-minsik <smin58810@gmail.com>
2023-11-08 19:14:54 +0900

    add: Solve BOJ #19942

--- py/19942.py
+++ py/19942.py
@@ -1,79 +1,35 @@
-# import sys
-# from itertools import combinations
-# input = sys.stdin.readline
-
-# n = int(input())
-# p, f, c, v = map(int, input().split())
-# arr = [list(map(int, input().split())) for _ in range(n)]
-# ret = 1e9
-
-# for i in range(1, n+1):
-#     for temp in combinations(arr, i):
-#         tp = 0
-#         tf = 0
-#         tc = 0
-#         tv = 0
-#         tm = 0
-#         for what in temp:
-#             tp += what[0]
-#             tf += what[1]
-#             tc += what[2]
-#             tv += what[3]
-#             tm += what[4]
-
-#             if tp >= p and tf >= f and tc >= c and tv >= v:
-#                 if ret > tm:
-#                     ret = tm
-#                     answer = []
-#                     for j in temp:
-#                         answer.append(arr.index(j) + 1)
-
-# if ret == 1e9:
-#     print(-1)
-# else:
-#     print(ret)
-#     print(answer)
-
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 mp, mf, ms, mv = map(int, input().split())
+food = [list(map(int, input().split())) for _ in range(n)]
+answer = 1e9
+buy = [99]
 
-food = []
-for _ in range(n):
-    food.append(list(map(int, input().split())))
 
-answer = {}
-cost = 1e9
 for i in range(1, 1 << n):
-    a, b, c, d, cur = 0, 0, 0, 0, 0
-    temp = []
+    sp, sf, ss, sv, sc = 0, 0, 0, 0, 0
+    pick = []
 
     for j in range(n):
         if i & (1 << j):
-            temp.append(j+1)
-            a += food[j][0]
-            b += food[j][1]
-            c += food[j][2]
-            d += food[j][3]
-            cur += food[j][4]
+            pick.append(j + 1)
+            sp += food[j][0]
+            sf += food[j][1]
+            ss += food[j][2]
+            sv += food[j][3]
+            sc += food[j][4]
 
-    if a >= mp and b >= mf and c >= ms and d >= mv and cur <= cost:
-        cost = cur
-        if cost in answer:
-            answer[cost].append([sorted(temp)])
-        else:
-            answer[cost] = sorted([temp])
+    if sp >= mp and sf >= mf and ss >= ms and sv >= mv:
+        if sc < answer:
+            answer = sc
+            buy = sorted(pick)
+        elif sc == answer:
+            buy = sorted(min(buy, pick))
 
-if cost == 1e9:
-    print(-1)
+if answer != 1e9:
+    print(answer)
+    print(*buy)
 else:
-    print(cost)
-    answer[cost].sort()
-    for q in answer[cost][0]:
-        print(q, end = " ")
-
-
-
+    print(-1)
