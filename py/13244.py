91c799039fa883da54d0be6173243e4dc0b326fd
k-minsik <smin58810@gmail.com>
2023-11-12 17:51:20 +0900

    refactor: Remind BOJ #13244

--- py/13244.py
+++ py/13244.py
@@ -1,32 +1,69 @@
+# import sys
+# input = sys.stdin.readline
+
+# def dfs(node, visited):
+#     visited[node] = 1
+#     for next in arr[node]:
+#         if not visited[next]:
+#             visited = dfs(next, visited)
+#     return visited
+
+# for _ in range(int(input())):
+#     n = int(input())
+#     e = int(input())
+#     arr = [[] for _ in range(n + 1)]
+
+#     for _ in range(e):
+#         a, b = map(int, input().split())
+#         arr[a].append(b)
+#         arr[b].append(a)
+
+#     if n != e + 1:
+#         print("graph")
+#         continue
+    
+#     visited = [False for _ in range(n + 1)]
+#     visited[0] = True
+#     dfs(1, visited)
+
+#     if False in visited:
+#         print("graph")
+#     else:
+#         print("tree")
+
+
 import sys
 input = sys.stdin.readline
 
-def dfs(node, visited):
-    visited[node] = 1
-    for next in arr[node]:
+def dfs(idx):
+    visited[idx] = True
+
+    for next in adjList[idx]:
         if not visited[next]:
-            visited = dfs(next, visited)
-    return visited
+            dfs(next)
 
-for _ in range(int(input())):
+tc = int(input())
+for _ in range(tc):
     n = int(input())
-    e = int(input())
-    arr = [[] for _ in range(n + 1)]
+    m = int(input())
+    adjList = [[] for _ in range(n)]
+    visited = [False] * n
 
-    for _ in range(e):
+    for _ in range(m):
         a, b = map(int, input().split())
-        arr[a].append(b)
-        arr[b].append(a)
+        a -= 1
+        b -= 1
 
-    if n != e + 1:
-        print("graph")
-        continue
-    
-    visited = [False for _ in range(n + 1)]
-    visited[0] = True
-    dfs(1, visited)
+        adjList[a].append(b)
+        adjList[b].append(a)
 
-    if False in visited:
-        print("graph")
-    else:
+    check = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            check += 1
+
+    if check == 1 and m == n - 1:
         print("tree")
+    else:
+        print("graph")
