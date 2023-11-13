970f58a1497e30048690ca1755a671905515185e
k-minsik <smin58810@gmail.com>
2023-11-13 23:46:07 +0900

    add: Solve BOJ #14469

--- py/14469.py
+++ py/14469.py
@@ -1,10 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-arr.sort()
+cows = [list(map(int, input().split())) for _ in range(n)]
 
-t = arr[0][0] + arr[0][1]
-for i in range(1, n):
-    t = max(t, arr[1][0])
-    t += arr[1][1]
+time = 0
+for enter, take in sorted(cows):
+    if enter <= time:
+        time += take
+    else:
+        time = enter + take
 
-print(t)
+print(time)
