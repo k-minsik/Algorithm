2a22969a5a994f66f71bb58027cc5cb7bd5b5ec5
k-minsik <smin58810@gmail.com>
2023-11-13 23:22:48 +0900

    refactor: Remind BOJ #3015

--- py/3015.py
+++ py/3015.py
@@ -1,24 +1,53 @@
+# # import sys
+# # input = sys.stdin.readline
+
+# # n = int(input())
+# # st = []
+# # answer = 0
+
+# # for _ in range(n):
+# #     now = int(input())
+# #     cnt = 1
+
+# #     while st and st[-1][0] <= now:
+# #         answer += st[-1][1]
+# #         if now == st[-1][0]:
+# #             cnt = st[-1][1] + 1
+# #         else:
+# #             cnt = 1
+        
+# #         st.pop()
+# #     if st:
+# #         answer += 1
+# #     st.append([now, cnt])
+
+# # print(answer)
+
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-st = []
+stack = []
 answer = 0
 
 for _ in range(n):
-    now = int(input())
-    cnt = 1
+    height = int(input())
 
-    while st and st[-1][0] <= now:
-        answer += st[-1][1]
-        if now == st[-1][0]:
-            cnt = st[-1][1] + 1
-        else:
-            cnt = 1
-        
-        st.pop()
-    if st:
-        answer += 1
-    st.append([now, cnt])
+    while stack and stack[-1][0] < height:
+        answer += stack.pop()[1]
+
+    if stack and stack[-1][0] == height:
+        count = stack.pop()[1]
+        answer += count
+
+        if stack:
+            answer += 1
+        stack.append((height, count + 1))
+
+    else:
+        if stack:
+            answer += 1
+        stack.append((height, 1))
 
 print(answer)
