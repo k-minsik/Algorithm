b1e1da26a2587ccbef257a3694fdd2b93f0daf92
k-minsik <smin58810@gmail.com>
2023-11-15 02:01:43 +0900

    refactor: Remind BOJ #1644

--- py/1644.py
+++ py/1644.py
@@ -1,29 +1,70 @@
-n = int(input())
+# n = int(input())
 
-prime = [2, 3]
-for i in range(4, n + 1):
-    flag = 0
+# prime = [2, 3]
+# for i in range(4, n + 1):
+#     flag = 0
+#     for j in range(2, int(i ** 0.5) + 1):
+#         if i % j == 0:
+#             flag = 1
+#             break
+#     if not flag:
+#         prime.append(i)
+            
+# answer, l, r = 0, 0, 0
+# temp = prime[l]
+# while l <= r and r < len(prime):
+#     if temp == n:
+#         answer += 1
+#         temp -= prime[l]
+#         l += 1
+#     elif temp < n:
+#         r += 1
+#         if r == len(prime):
+#             break
+#         temp += prime[r]
+#     elif temp > n:
+#         temp -= prime[l]
+#         l += 1
+    
+# print(answer)
+
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+primeNum = [2, 3]
+for i in range(5, n + 1):
+    valid = True
     for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
-            flag = 1
+            valid = False
             break
-    if not flag:
-        prime.append(i)
-            
-answer, l, r = 0, 0, 0
-temp = prime[l]
-while l <= r and r < len(prime):
+
+    if valid:
+        primeNum.append(i)
+
+answer = 0
+s, e = 0, 0
+temp = primeNum[s]
+while s <= e:
     if temp == n:
         answer += 1
-        temp -= prime[l]
-        l += 1
-    elif temp < n:
-        r += 1
-        if r == len(prime):
+        e += 1
+        if e == len(primeNum):
             break
-        temp += prime[r]
-    elif temp > n:
-        temp -= prime[l]
-        l += 1
+        temp -= primeNum[s]
+        temp += primeNum[e]
+        s += 1
     
+    elif temp > n:
+        temp -= primeNum[s]
+        s += 1
+
+    elif temp < n:
+        e += 1
+        if e == len(primeNum):
+            break
+        temp += primeNum[e]
+
 print(answer)
