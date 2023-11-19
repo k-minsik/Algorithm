81a592ea4315fa3a21fdca1aefd11e0b0ef79cab
k-minsik <smin58810@gmail.com>
2023-11-19 19:56:20 +0900

    refactor: Remind BOJ #13144

--- py/13144.py
+++ py/13144.py
@@ -1,21 +1,46 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# visited = [False for _ in range(100001)]
+
+# answer = 0
+# l, r = 0, 0
+
+# while r < n:
+#     if not visited[arr[r]]:
+#         visited[arr[r]] = True
+#         r += 1
+#     else:
+#         answer += r - l
+#         visited[arr[l]] = False
+#         l += 1
+
+# answer += (r - l) * (r - l + 1) // 2
+# print(answer)
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int, input().split()))
-visited = [False for _ in range(100001)]
+nums = list(map(int, input().split()))
+visited = [False] * 100001
 
 answer = 0
-l, r = 0, 0
+s, e = 0, 0
+while True:
+    if e >= n:
+        break
 
-while r < n:
-    if not visited[arr[r]]:
-        visited[arr[r]] = True
-        r += 1
+    if visited[nums[e]]:
+        visited[nums[s]] = False
+        answer += e - s
+        s += 1
+
     else:
-        answer += r - l
-        visited[arr[l]] = False
-        l += 1
+        visited[nums[e]] = True
+        e += 1
 
-answer += (r - l) * (r - l + 1) // 2
+answer += (e - s) * (e - s + 1) // 2
 print(answer)
