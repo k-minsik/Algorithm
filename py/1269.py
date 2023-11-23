9dafb4ce5891be41a37ac5bd1107cec96ba0b4ca
k-minsik <smin58810@gmail.com>
2023-11-24 01:41:00 +0900

    refactor: Resolve BOJ #1269

--- py/1269.py
+++ py/1269.py
@@ -1,11 +1,30 @@
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+
+# arr = set(map(int, input().split()))
+# arr2 = set(map(int, input().split()))
+# temp = len(arr & arr2)
+
+# print(arr & arr2)
+# print(len(arr) + len(arr2) - temp * 2)
+
+
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+a, b = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-arr = set(map(int, input().split()))
-arr2 = set(map(int, input().split()))
-temp = len(arr & arr2)
+d = {}
+for i in A:
+    d[i] = 1
 
-print(arr & arr2)
-print(len(arr) + len(arr2) - temp * 2)
+answer = a + b
+for i in B:
+    if i in d:
+        answer -= 2
+
+print(answer)
