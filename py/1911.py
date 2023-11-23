3959493a360a0e32c1ab2a0f93244670317f15c9
k-minsik <smin58810@gmail.com>
2023-11-23 18:36:15 +0900

    refactor: Resolve BOJ #1911

--- py/1911.py
+++ py/1911.py
@@ -1,22 +1,41 @@
-import sys
+# import sys
+# input = sys.stdin.readline
+
+# n, l = map(int, input().split())
+# arr = sorted([list(map(int, input().split())) for _ in range(n)])
+
+# answer = 0
+# idx = 0
+
+# for s, e in arr:
+#     if s > idx:
+#         idx = s
+    
+#     dist = e - idx
+#     temp = dist // l
+#     if dist % l:
+#         temp += 1
+
+#     idx += l * temp
+#     answer += temp
+
+# print(answer)
+
+import sys, math
 input = sys.stdin.readline
 
 n, l = map(int, input().split())
-arr = sorted([list(map(int, input().split())) for _ in range(n)])
+hole = sorted([list(map(int, input().split())) for _ in range(n)])
 
-answer = 0
-idx = 0
+answer, endPoint = 0, 0
+for s, e in hole:
+    if s > endPoint:
+        endPoint = s
 
-for s, e in arr:
-    if s > idx:
-        idx = s
-    
-    dist = e - idx
-    temp = dist // l
-    if dist % l:
-        temp += 1
+    holeLength = e - endPoint
+    need = math.ceil(holeLength / l)
 
-    idx += l * temp
-    answer += temp
+    endPoint += l * need
+    answer += need
 
 print(answer)
