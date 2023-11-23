b23678f38e7ba6ed9441a0f16132d1d49b545fad
k-minsik <smin58810@gmail.com>
2023-11-23 23:53:14 +0900

    refactor: Resolve BOJ #2170

--- py/2170.py
+++ py/2170.py
@@ -1,17 +1,39 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+# arr.sort()
+
+# s,e = arr[0]
+# answer = 0
+# for i in range(1, n):
+#     if arr[i][0] > e:
+#         answer += e - s
+#         s, e = arr[i]
+#     elif arr[i][1] > e:
+#         e = arr[i][1]
+
+# print(answer + e - s)
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-arr.sort()
 
-s,e = arr[0]
+line = [list(map(int, input().split())) for _ in range(n)]
+line.sort()
+
 answer = 0
-for i in range(1, n):
-    if arr[i][0] > e:
-        answer += e - s
-        s, e = arr[i]
-    elif arr[i][1] > e:
-        e = arr[i][1]
+endPoint = -1e10
+for s, e in line:
+    if e < endPoint:
+        continue
 
-print(answer + e - s)
+    if s > endPoint:
+        endPoint = s
+    
+    answer += e - endPoint
+    endPoint = e
+
+print(answer)
