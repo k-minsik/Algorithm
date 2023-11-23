61cb76be8d3c1ce67fd3afafa03fbfab97224bfb
k-minsik <smin58810@gmail.com>
2023-11-24 02:00:41 +0900

    refactor: Resolve BOJ #7795

--- py/7795.py
+++ py/7795.py
@@ -1,25 +1,46 @@
+# import sys
+# input = sys.stdin.readline
+
+# tk = int(input())
+# for _ in range(tk):
+#     n, m = map(int, input().split())
+#     A = sorted(list(map(int, input().split())))
+#     B = sorted(list(map(int, input().split())))
+
+#     answer = 0
+#     for a in A:
+#         temp = 0
+#         lo, hi = 0, m - 1
+#         while lo <= hi:
+#             mid = (lo + hi) // 2
+
+#             if a > B[mid]:
+#                 temp = mid + 1
+#                 lo = mid + 1
+#             else:
+#                 hi = mid - 1
+
+#         answer += temp
+
+#     print(answer)
+
+
 import sys
 input = sys.stdin.readline
 
-tk = int(input())
-for _ in range(tk):
-    n, m = map(int, input().split())
+tc = int(input())
+for _ in range(tc):
+    a, b = map(int, input().split())
     A = sorted(list(map(int, input().split())))
-    B = sorted(list(map(int, input().split())))
+    B = sorted(list(map(int, input().split()))) + [int(1e9)]
 
     answer = 0
-    for a in A:
-        temp = 0
-        lo, hi = 0, m - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-
-            if a > B[mid]:
-                temp = mid + 1
-                lo = mid + 1
-            else:
-                hi = mid - 1
-
-        answer += temp
-
+    savePoint = 0
+    for i in range(a):
+        for j in range(savePoint, b + 1):
+            if A[i] <= B[j]:
+                savePoint = j
+                answer += savePoint
+                break        
+            
     print(answer)
