7ee5889cf32d901d77a72d1f30aa1a74b8f66a5a
k-minsik <smin58810@gmail.com>
2023-11-24 15:38:51 +0900

    refactor: Resolve BOJ #14627

--- py/14627.py
+++ py/14627.py
@@ -1,19 +1,44 @@
+# import sys
+# input = sys.stdin.readline
+
+# s, c = map(int, input().split())
+# arr = [int(input()) for _ in range(s)]
+# lo, hi = 1, max(arr)
+
+# answer = 0
+# while lo <= hi:
+#     mid = (lo + hi) // 2
+
+#     cnt = sum(i // mid for i in arr)
+#     if cnt >= c:
+#         lo = mid + 1
+#         answer = mid
+#     else:
+#         hi = mid - 1
+
+# print(sum(arr) - (answer * c))
+
 import sys
 input = sys.stdin.readline
 
-s, c = map(int, input().split())
-arr = [int(input()) for _ in range(s)]
-lo, hi = 1, max(arr)
+n, k = map(int, input().split())
+pas = [int(input()) for _ in range(n)]
+l, r = 1, max(pas)
 
-answer = 0
-while lo <= hi:
-    mid = (lo + hi) // 2
+while l <= r:
+    mid = (l + r) // 2
 
-    cnt = sum(i // mid for i in arr)
-    if cnt >= c:
-        lo = mid + 1
+    make = 0
+    for pa in pas:
+        make += pa // mid
+
+        if make > k:
+            break
+    
+    if make >= k:
+        l = mid + 1
         answer = mid
-    else:
-        hi = mid - 1
+    elif make < k:
+        r = mid - 1
 
-print(sum(arr) - (answer * c))
+print(sum(pas) - k * answer)
