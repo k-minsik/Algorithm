5ae07bb375f932619a77786a850154ad84ca69d9
k-minsik <smin58810@gmail.com>
2023-11-26 17:01:10 +0900

    refactor: Resolve BOJ #17070

--- py/17070.py
+++ py/17070.py
@@ -21,10 +21,30 @@
 #     answer += dp[i][n-1][n-1]
 # print(answer)
 
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-a = sorted(list(map(int, input().split())))
-b = sorted(list(map(int, input().split())), reverse= True)
+graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0] * n for _ in range(n)] for _ in range(3)]
+dp[0][0][1] = 1
+
+for x in range(2, n):
+    if graph[0][x] == 0:
+        dp[0][0][x] = 1
+    else:
+        break
+
+for y in range(1, n):
+    for x in range(2, n):
+        if not(graph[y-1][x-1] or graph[y][x-1] or graph[y-1][x] or graph[y][x]):
+            dp[1][y][x] = dp[0][y-1][x-1] + dp[1][y-1][x-1] + dp[2][y-1][x-1]
+        
+        if not graph[y][x]:
+            dp[0][y][x] = dp[0][y][x-1] + dp[1][y][x-1]
+            dp[2][y][x] = dp[1][y-1][x] + dp[2][y-1][x]
+
 answer = 0
-for i, j in zip(a, b):
-    answer += i * j
+for i in range(3):
+    answer += dp[i][n-1][n-1]
 print(answer)
