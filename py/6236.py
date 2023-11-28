128fad05d405c3ccb3200659367882c68c62c632
k-minsik <smin58810@gmail.com>
2023-11-28 18:54:19 +0900

    refactor: Resolve BOJ #6236

--- py/6236.py
+++ py/6236.py
@@ -1,40 +1,74 @@
+# import sys
+# input = sys.stdin.readline
+
+# def check(mid):
+#     global n, m
+
+#     now = mid
+#     cnt = 1
+#     for i in range(n):
+#         if now - arr[i] < 0:
+#             now = mid
+#             cnt += 1
+        
+#         now -= arr[i]
+        
+#     if cnt <= m:
+#         return True
+#     else:
+#         return False
+
+
+# n, m = map(int, input().split())
+# arr = []
+# for _ in range(n):
+#     arr.append(int(input()))
+
+# low = max(arr)
+# high = int(1e10)
+
+# answer = low
+# while low <= high:
+#     mid = (low + high) // 2
+
+#     if check(mid):
+#         answer = mid
+#         high = mid - 1
+#     else:
+#         low = mid + 1
+
+# print(answer)
+
 import sys
 input = sys.stdin.readline
 
-def check(mid):
-    global n, m
-
-    now = mid
-    cnt = 1
-    for i in range(n):
-        if now - arr[i] < 0:
-            now = mid
-            cnt += 1
-        
-        now -= arr[i]
-        
-    if cnt <= m:
-        return True
-    else:
-        return False
-
-
 n, m = map(int, input().split())
-arr = []
-for _ in range(n):
-    arr.append(int(input()))
+money = [int(input()) for _ in range(n)]
 
-low = max(arr)
-high = int(1e10)
+M = max(money)
+low = M
+high = int(1e9)
+answer = M 
 
-answer = low
 while low <= high:
     mid = (low + high) // 2
+    if mid < M:
+        low = mid + 1
 
-    if check(mid):
-        answer = mid
+    count = 1
+    have = mid
+    for i in range(n):
+        if have - money[i] < 0:
+            have = mid
+            count += 1
+
+        have -= money[i]
+
+    if count <= m:
         high = mid - 1
-    else:
+        answer = mid
+
+    elif count > m:
         low = mid + 1
 
 print(answer)
