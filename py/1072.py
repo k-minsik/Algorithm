0f016634670d5b544be8af25fbc29741ce8dd732
k-minsik <smin58810@gmail.com>
2023-11-30 00:43:20 +0900

    refactor: Resolve BOJ #1072

--- py/1072.py
+++ py/1072.py
@@ -1,20 +1,49 @@
+# x, y = map(int, input().split())
+# z = y * 100 // x
+
+# if z >= 99:
+#     print(-1)
+# else:
+#     answer = 0
+#     lo = 1
+#     hi = x
+#     while lo <= hi:
+#         mid = (lo + hi) // 2
+#         temp = (y + mid) * 100 // (x + mid)
+
+#         if temp > z:
+#             answer = mid
+#             hi = mid - 1
+#         else:
+#             lo = mid + 1
+
+#     print(answer)
+
+
+import sys
+input = sys.stdin.readline
+
 x, y = map(int, input().split())
-z = y * 100 // x
+winRate = int(y * 100 / x)
 
-if z >= 99:
+if winRate >= 99:
     print(-1)
-else:
-    answer = 0
-    lo = 1
-    hi = x
-    while lo <= hi:
-        mid = (lo + hi) // 2
-        temp = (y + mid) * 100 // (x + mid)
+    exit()
 
-        if temp > z:
-            answer = mid
-            hi = mid - 1
-        else:
-            lo = mid + 1
+answer = -1
+low, high = 1, int(1e9)
+while low <= high:
+    mid = (low + high) // 2
 
-    print(answer)
+    total = x + mid
+    win = y + mid
+    rate = int(win * 100 / total)
+
+    if rate != winRate:
+        high = mid - 1
+        answer = mid
+
+    else:
+        low = mid + 1
+
+print(answer)
