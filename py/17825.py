57cfed3b0b110a5f7a29cfdd856b1f5185026090
k-minsik <smin58810@gmail.com>
2023-11-30 19:59:23 +0900

    add: Solve BOJ #17825

--- py/17825.py
+++ py/17825.py
@@ -1,52 +1,38 @@
-graph = [[i + 1] for i in range(21)] + [[] for _ in range(12)]
-graph[5].append(22), graph[10].append(25), graph[15].append(27)
-graph[22].append(23), graph[23].append(24), graph[24].append(30)
-graph[25].append(26), graph[26].append(30)
-graph[27].append(28), graph[28].append(29), graph[29].append(30)
-graph[30].append(31), graph[31].append(32), graph[32].append(20)
+import sys
+input = sys.stdin.readline
 
-score = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 
-         30, 32, 34, 36, 38, 40, 0, 13, 16, 19, 22, 24, 29, 27,
-         26, 25, 30, 35]
-visited = [0 for _ in range(33)]
+def dfs(round, tempAnswer):
+    global answer
+
+    if round == 10:
+        answer = max(answer, tempAnswer)
+        return
+    
+    for i in range(4):
+        x, initX = horse[i], horse[i]
+        if x == 21:
+            continue
+
+        move = dice[round]
+        if x in [5, 10, 15]:
+            x = graph[x][1]
+            move -= 1
+        
+        for _ in range(move):
+            x = graph[x][0]
+
+        if x == 21 or (x not in horse):
+            horse[i] = x
+            dfs(round + 1, tempAnswer + score[x])
+            horse[i] = initX
+        
+graph = [[1],[2],[3],[4],[5],[6,22],[7],[8],[9],[10],[11,29],[12],[13],[14],[15],[16,28],[17],[18],[19],[20],[21],[21],
+         [23],[24],[25],[31],[25],[26],[27],[30],[25],[32],[20]]
+score = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 0,
+         13, 16, 19, 25, 26, 27, 28, 22, 24, 30, 35]
 
 dice = list(map(int, input().split()))
 horse = [0, 0, 0, 0]
 answer = 0
-
-def go(cnt, temp):
-    global answer
-    if cnt == 10:
-        answer = max(answer, temp)
-        return
-    
-    for i in range(4):
-        if horse[i] == 21:
-            continue
-        now = horse[i]
-
-        move = dice[cnt]
-        if len(graph[horse[i]]) == 2:
-            horse[i] = graph[horse[i]][1]
-            move -= 1
-
-        while move > 0:
-            if horse[i] == 21:
-                break
-            horse[i] = graph[horse[i]][0]
-            move -= 1
-        
-        if horse[i] != 21 and visited[horse[i]] == 1:
-            visited[now] = 1
-            horse[i] = now
-            continue
-        else:
-            visited[now] = 0
-            visited[horse[i]] = 1
-            go(cnt + 1, temp + score[horse[i]])
-            visited[horse[i]] = 0
-            horse[i] = now
-            visited[now] = 1
-
-go(0, 0)
+dfs(0, 0)
 print(answer)
