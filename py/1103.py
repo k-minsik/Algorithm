72ffa51687f8424e29c69d84e798f03958e21e8b
k-minsik <smin58810@gmail.com>
2023-12-02 21:52:31 +0900

    refactor: Resolve BOJ #1103

--- py/1103.py
+++ py/1103.py
@@ -1,33 +1,71 @@
+# import sys
+# sys.setrecursionlimit(100000)
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# arr = [list(input().rstrip()) for _ in range(n)]
+# visited = [[0 for _ in range(m)] for _ in range(n)]
+# dp = [[0 for _ in range(m)] for _ in range(n)]
+# dy = [-1, 0, 1, 0]
+# dx = [0, 1, 0, -1]
+# answer = 0
+
+# def dfs(y, x, cnt):
+#     global answer
+#     answer = max(answer, cnt)
+#     for i in range(4):
+#         ny = y + dy[i] * int(arr[y][x])
+#         nx = x + dx[i] * int(arr[y][x])
+
+#         if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] != 'H':
+#             if cnt + 1 > dp[ny][nx]:
+#                 if visited[ny][nx]:
+#                     print(-1)
+#                     exit()
+#                 else:
+#                     visited[ny][nx] = 1
+#                     dp[ny][nx] = cnt + 1
+#                     dfs(ny, nx, cnt + 1)
+#                     visited[ny][nx] = 0
+
+
+# dfs(0, 0, 0)
+# print(answer + 1)
+
 import sys
-sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-arr = [list(input().rstrip()) for _ in range(n)]
-visited = [[0 for _ in range(m)] for _ in range(n)]
-dp = [[0 for _ in range(m)] for _ in range(n)]
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
-answer = 0
+
+r, c = map(int, input().split())
+graph = [list(input().strip()) for _ in range(r)]
+visited = [[False] * c for _ in range(r)]
+dp = [[0] * c for _ in range(r)]
+visited[0][0] = True
 
 def dfs(y, x, cnt):
     global answer
-    answer = max(answer, cnt)
-    for i in range(4):
-        ny = y + dy[i] * int(arr[y][x])
-        nx = x + dx[i] * int(arr[y][x])
 
-        if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] != 'H':
-            if cnt + 1 > dp[ny][nx]:
-                if visited[ny][nx]:
+    move = int(graph[y][x])
+    for i in range(4):
+        ny = y + dy[i] * move
+        nx = x + dx[i] * move
+
+        if 0 <= ny < r and 0 <= nx < c and graph[ny][nx] != 'H':
+            if cnt > dp[ny][nx]:
+                if not visited[ny][nx]:
+                    visited[ny][nx] = True
+                    dp[ny][nx] = cnt
+                    dfs(ny, nx, cnt + 1)
+                    visited[ny][nx] = False
+
+                else:
                     print(-1)
                     exit()
-                else:
-                    visited[ny][nx] = 1
-                    dp[ny][nx] = cnt + 1
-                    dfs(ny, nx, cnt + 1)
-                    visited[ny][nx] = 0
+        else:
+            answer = max(answer, cnt)
 
-
-dfs(0, 0, 0)
-print(answer + 1)
+answer = -1
+dfs(0, 0, 1)
+print(answer)
