fac89edc4df9248445a513aee4aa3501156b7363
k-minsik <smin58810@gmail.com>
2023-12-27 23:54:22 +0900

    refactor: Resolve BOJ #2293

--- py/2293.py
+++ py/2293.py
@@ -1,3 +1,19 @@
+# import sys
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+# coin = [int(input()) for _ in range(n)]
+# dp = [0] * (k + 1)
+
+# for c in coin:
+#     for i in range(k + 1):
+#         if i == c:
+#             dp[i] += 1
+#         elif i > c:
+#             dp[i] += dp[i - c]
+
+# print(dp[k])
+
 import sys
 input = sys.stdin.readline
 
@@ -6,10 +22,10 @@ coin = [int(input()) for _ in range(n)]
 dp = [0] * (k + 1)
 
 for c in coin:
-    for i in range(k + 1):
-        if i == c:
-            dp[i] += 1
-        elif i > c:
-            dp[i] += dp[i - c]
+    for m in range(k + 1):
+        if m == c:
+            dp[m] += 1
+        elif m > c:
+            dp[m] += dp[m - c]
 
 print(dp[k])
