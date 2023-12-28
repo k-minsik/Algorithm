53489a88e9b2450a35ce99e3a69ee03c7a2ba6dc
k-minsik <smin58810@gmail.com>
2023-12-28 21:52:27 +0900

    refactor: Resolve BOJ #12865

--- py/12865.py
+++ py/12865.py
@@ -1,11 +1,26 @@
+# import sys
+# input = sys.stdin.readline
+
+# n, k = map(int, input().split())
+# dp = [0 for _ in range(k + 1)]
+
+# for _ in range(n):
+#     w, v = map(int, input().split())
+#     for i in range(k, w - 1, -1):
+#         dp[i] = max(dp[i], dp[i - w] + v)
+
+# print(dp[k])
+
+
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-dp = [0 for _ in range(k + 1)]
+dp = [0] * (k + 1)
 
 for _ in range(n):
     w, v = map(int, input().split())
+    
     for i in range(k, w - 1, -1):
         dp[i] = max(dp[i], dp[i - w] + v)
 
