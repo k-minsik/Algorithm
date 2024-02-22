a89b4a0219c27cecfffc6a72034f8daa6da1b8b5
k-minsik <smin58810@gmail.com>
2024-02-22 18:28:06 +0900

    add: Solve BOJ #2623

---
+++ py/2623.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+order = [list(map(int, input().split()))[1:] for _ in range(m)]
+
+answer = []
+graph = [[] for _ in range(n + 1)]
+degree = [0] * (n + 1)
+dq = deque()
+
+for o in order:
+    for i in range(len(o) - 1):
+        graph[o[i]].append(o[i + 1])
+        degree[o[i + 1]] += 1
+
+for g in graph:
+    print(g)
+
+for i in range(1, n + 1):
+    if degree[i] == 0:
+        dq.append(i)
+
+while dq:
+    before = dq.popleft()
+    answer.append(before)
+
+    for after in graph[before]:
+        degree[after] -= 1
+
+        if degree[after] == 0:
+            dq.append(after)
+
+
+if len(answer) == n:
+    for i in answer:
+        print(i)
+else:
+    print(0)
--- py/연속된부분수열.py
+++ py/연속된부분수열.py
@@ -1,36 +1,20 @@
 def solution(sequence, k):
     answer = [0, int(1e9)]
     
-    l, r, t = 0, 0, sequence[0]
-    sequence += [0]
-    
-    while r < len(sequence) - 1:
-        print(l, r, t)
-        if t < k:
-            r += 1
-            t += sequence[r]
-        
-        elif t == k:
-            before = answer[1] - answer[0]
-            now = r - l
-            
-            if now < before:
-                answer[0] = l
-                answer[1] = r
+    l, r = 0, 0
+    t = sequence[0]
 
-            t -= sequence[l]
-            l += 1
+    while r < len(sequence):
+        if t < k and r < len(sequence) - 1:
             r += 1
             t += sequence[r]
-        
-        elif t > k:
+        elif t >= k:
+            if t == k:
+                if r - l < answer[1] - answer[0]:
+                    answer = [l, r]
             t -= sequence[l]
             l += 1
-            
-        
+        else:
+            break
+
     return answer
-
-# 	[2, 3]
-# 	[6, 6]
-# [2, 2, 2, 2, 2]	6
-print(solution([1, 1, 1, 2, 3, 4, 5], 5))
