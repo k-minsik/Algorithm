037de6e39eb6915eefb47496a050f2bc7c7ce945
k-minsik <smin58810@gmail.com>
2024-02-25 00:29:46 +0900

    add: Solve PGS 2022 [PCCP 모의고사 #1] 1번 - 외톨이 알파벳

--- java/programmers/src/외톨이알파벳.java
+++ java/programmers/src/외톨이알파벳.java
@@ -1,31 +1,33 @@
 import java.util.*;
 
-class Solution {
-    public String solution(String input) {
-        String answer = "";
-        ArrayList<String> store = new ArrayList<>();
-        Map<String, Integer> check = new HashMap<>();
+public class 외톨이알파벳 {
+    class Solution {
+        public String solution(String input) {
+            String answer = "";
+            ArrayList<String> store = new ArrayList<>();
+            Map<String, Integer> check = new HashMap<>();
 
-        String[] str = input.split("");
-        for (int i = 0; i < str.length; i++) {
-            if (!check.containsKey(str[i])) {
-                check.put(str[i], i);
-            } else {
-                int before = check.get(str[i]);
-                if (i - before == 1) {
+            String[] str = input.split("");
+            for (int i = 0; i < str.length; i++) {
+                if (!check.containsKey(str[i])) {
                     check.put(str[i], i);
                 } else {
-                    if (!store.contains(str[i]))
-                        store.add(str[i]);
+                    int before = check.get(str[i]);
+                    if (i - before == 1) {
+                        check.put(str[i], i);
+                    } else {
+                        if (!store.contains(str[i]))
+                            store.add(str[i]);
+                    }
                 }
             }
-        }
 
-        Collections.sort(store);
-        for (String s : store) {
-            answer += s;
-        }
+            Collections.sort(store);
+            for (String s : store) {
+                answer += s;
+            }
 
-        return answer.equals("") ? "N" : answer;
+            return answer.equals("") ? "N" : answer;
+        }
     }
 }
