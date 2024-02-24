7da42ffc6abd7bf96a30bb4cc46b5d12001d1321
k-minsik <smin58810@gmail.com>
2024-02-25 01:49:04 +0900

    add: Solve PGS 멀리뛰기

---
+++ java/programmers/src/멀리뛰기.java
@@ -0,0 +1,15 @@
+public class 멀리뛰기 {
+    class Solution {
+        public long solution(int n) {
+            long[] answer = new long[n + 2];
+            answer[1] = 1;
+            answer[2] = 2;
+
+            for (int i = 3; i <= n; i++) {
+                answer[i] = (answer[i-1] + answer[i-2]) % 1234567;
+            }
+
+            return answer[n];
+        }
+    }
+}
